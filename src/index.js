f8e7961cd642ecd4ace929c2a14e3acb389036dc
Liudmila Krystsinevich <lkristinevich.webdev@gmail.com>
2020-03-23 14:56:56 +0100

    zaliczenie

---
+++ src/index.js
@@ -0,0 +1,161 @@
+//Navigation responsive 
+document.querySelector(".icon").addEventListener('click', myFunction)
+function myFunction() {
+  var x = document.querySelector(".navigation");
+  if (x.className === "navigation") {
+    x.className += " responsive";
+  } else {
+    x.className = "navigation";
+  }
+}
+
+//Text typing
+var text = document.getElementById("text");
+var newtext = "Hire a great web developer";
+var index = 1;
+
+setInterval(writeText, 100);
+
+function writeText() {
+  text.innerText = newtext.slice(0, index)
+    
+  index++;
+	
+	if(index > newtext.length){
+    index = 1;
+  }
+
+}
+
+// Canvas
+
+var canvas = document.querySelector('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+   
+var c = canvas.getContext( '2d' );
+
+var mouse={
+  x: undefined,
+  y: undefined
+}
+
+var maxRadius = 40;
+// var minRadius = 2;
+
+var colorArray=[
+  '#a1c9d0',
+  '#6eacb7',	
+  '#e2b7b9',	
+  '#fdcece',	
+  '#cc8084',
+];
+
+window.addEventListener('mousemove', function(event){
+  mouse.x = event.x;
+  mouse.y = event.y;
+  console.log(mouse);
+  
+})
+
+window.addEventListener('resize', function(event){
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  
+  init();
+  
+})
+
+function Circle(x, y, dx, dy, radius){
+  this.x = x;
+  this.y = y;
+  this.dx = dx;
+  this.dy = dy;
+  this.radius = radius;
+  this.minRadius = radius;
+  this.color=colorArray[Math.floor(Math.random() * colorArray.length)]
+  
+  this.draw = function(){
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.fillStyle=this.color;
+    c.fill();
+  }
+  
+  this.update = function(){
+    if( this.x + this.radius > innerWidth || this.x - this.radius < 0){
+    this.dx = -this.dx;
+    }
+  
+    if(this.y + this.radius > innerHeight ||this.y - this.radius < 0){
+    this.dy = -this.dy;
+    }
+    
+   this.x += this.dx;
+   this.y += this.dy;
+    
+//     interactivity
+    if (mouse.x - this.x < 50 && mouse.x - this.x > -50 && mouse.y - this.y < 50 && mouse.y - this.y > -50
+      ) {
+        if(this.radius < maxRadius){
+          this.radius +=1;
+      }      
+    } else if (this.radius > this.minRadius) {
+        this.radius -=1
+       }
+    
+   this.draw();
+    
+  } 
+}
+
+var circleArray=[];
+
+function init(){
+  
+  circleArray=[]
+  
+  for(var i=0; i<800; i++) {
+  var radius = Math.random() * 3 + 1;
+  var x = Math.random() * (innerWidth - radius*2) + radius;
+  var y = Math.random() * (innerHeight- radius*2) + radius;
+  var dx = (Math.random() - 0.5);
+  var dy = (Math.random() - 0.5);
+  circleArray.push(new Circle(x,y,dx,dy, radius))
+
+  }
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+  c.clearRect(0, 0, innerWidth, innerHeight);
+  
+  for (var i=0; i<circleArray.length; i++){
+    circleArray[i].update();
+  }
+  
+}
+
+init()
+
+animate()
+
+//Scroll button
+
+var scroll_button = document.getElementById("scroll_button");
+
+window.addEventListener('scroll', scrollFunction)
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scroll_button.classList.add("visable");
+  } else {
+    scroll_button.classList.remove("visable");
+
+  }
+}
+scroll_button.addEventListener('click', topFunction)
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
